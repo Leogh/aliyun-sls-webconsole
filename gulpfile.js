c31d5dc1b0882792d6d0ad9dd60203246f6aed8a
Roman Lo <romam.nare@gmail.com>
2016-04-08 07:38:54 +0000

    Add utilities
    * Add eslint support.
    * Add gulp support.
    * Add passport support for the auth features.
    * Add log4js for logging feature.
    * Add mongoose as mongodb connector.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp"),
+    jade = require("gulp-jade"),
+    eslint = require("gulp-eslint"),
+    paths = {
+      'scripts': [
+        'public/javascripts/**/*.js',
+        '!node_modules/**',
+        '!public/javascripts/libs/**'
+      ],
+    };
+
+gulp.task('lint', function () {
+  return gulp.src(paths.scripts)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint'], function () {
+  console.log('Ready to Go!');
+});
