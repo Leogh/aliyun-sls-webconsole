87931d14b59a5faaa2233116b5f96cc2b6dc64b5
Roman Lo <romam.nare@gmail.com>
2016-04-08 10:24:13 +0000

    Update .gitignore
    Update: ignore c9 only config.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var aliyunSLS = require('./routes/aliyun/sls');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/aliyun-sls', aliyunSLS);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
